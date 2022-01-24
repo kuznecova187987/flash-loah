4b00a3cd65e9b84904f09dccc3840b2d9704896a
kuznecova187987 <88832115+kuznecova187987@users.noreply.github.com>
2022-01-24 23:34:25 +0300

    Add files via upload

---
+++ dist/index.spec.js
@@ -0,0 +1,160 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var forta_agent_1 = require("forta-agent");
+var _1 = __importDefault(require("."));
+var web3_1 = __importDefault(require("web3"));
+var web3 = new web3_1.default(forta_agent_1.getJsonRpcUrl());
+describe('flash loan agent', function () {
+    var handleTransaction;
+    var createTxEvent = function (_a) {
+        var gasUsed = _a.gasUsed, addresses = _a.addresses, logs = _a.logs, blockNumber = _a.blockNumber;
+        var tx = {};
+        var receipt = { gasUsed: gasUsed, logs: logs };
+        var block = { number: blockNumber };
+        var addressez = __assign({}, addresses);
+        return new forta_agent_1.TransactionEvent(forta_agent_1.EventType.BLOCK, forta_agent_1.Network.GOERLI, tx, receipt, [], addressez, block);
+    };
+    beforeAll(function () {
+        handleTransaction = _1.default.provideHandleTransaction(web3);
+    });
+    describe('handleTransaction', function () {
+        it('returns a finding if a flash loan attack is detected', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var flashLoanTopic, mockAddress, data, flashLoanEvent, protocolAddress, blockNumber, txEvent, findings;
+            var _a;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        flashLoanTopic = '0x631042c832b07452973831137f2d73e395028b44b250dedc5abb0ee766e168ac';
+                        mockAddress = [
+                            '0xB44ddb00B19a2F20E3c1C31A14C8965D75a8e4De',
+                            '0xfD6b4BF0b1DeA54D6A684F3044EFd59fCDD2C3aC',
+                            '0x99cD1385f95CC5Bd3F88E542F8fcF8A675c291FC',
+                        ];
+                        data = web3.eth.abi.encodeParameters(['address', 'address', 'address', 'uint256', 'uint256', 'uint16'], [mockAddress[0], mockAddress[1], mockAddress[2], 10000, 10000, 500]);
+                        flashLoanEvent = {
+                            topics: [flashLoanTopic],
+                            data: data,
+                        };
+                        protocolAddress = '0xacd43e627e64355f1861cec6d3a6688b31a6f952';
+                        blockNumber = 100;
+                        txEvent = createTxEvent({
+                            gasUsed: '7000001',
+                            addresses: (_a = {
+                                    '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9': true
+                                },
+                                _a[protocolAddress] = true,
+                                _a),
+                            logs: [flashLoanEvent],
+                            blockNumber: blockNumber,
+                        });
+                        return [4 /*yield*/, handleTransaction(txEvent)];
+                    case 1:
+                        findings = _b.sent();
+                        expect(findings).toStrictEqual([]);
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+        it('returns a finding if a flash loan attack is detected', function () { return __awaiter(void 0, void 0, void 0, function () {
+            var flashLoanTopic, mockAddress, data, flashLoanEvent, protocolAddress, blockNumber, txEvent, findings;
+            var _a;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        flashLoanTopic = '0x631042c832b07452973831137f2d73e395028b44b250dedc5abb0ee766e168ac';
+                        mockAddress = [
+                            '0xB44ddb00B19a2F20E3c1C31A14C8965D75a8e4De',
+                            '0xfD6b4BF0b1DeA54D6A684F3044EFd59fCDD2C3aC',
+                            '0x99cD1385f95CC5Bd3F88E542F8fcF8A675c291FC',
+                        ];
+                        data = web3.eth.abi.encodeParameters(['address', 'address', 'address', 'uint256', 'uint256', 'uint16'], [mockAddress[0], mockAddress[1], mockAddress[2], 100000, 100000, 5000]);
+                        flashLoanEvent = {
+                            topics: [flashLoanTopic],
+                            data: data,
+                        };
+                        protocolAddress = '0xacd43e627e64355f1861cec6d3a6688b31a6f952';
+                        blockNumber = 100;
+                        txEvent = createTxEvent({
+                            gasUsed: '7000001',
+                            addresses: (_a = {
+                                    '0x7d2768de32b0b80b7a3454c06bdac94a69ddc7a9': true
+                                },
+                                _a[protocolAddress] = true,
+                                _a),
+                            logs: [flashLoanEvent],
+                            blockNumber: blockNumber,
+                        });
+                        return [4 /*yield*/, handleTransaction(txEvent)];
+                    case 1:
+                        findings = _b.sent();
+                        expect(findings).toStrictEqual([
+                            forta_agent_1.Finding.fromObject({
+                                name: 'Flash Loan with Huge Amount Detection',
+                                description: "Flash Loan with huge amount of 100000 detected for " + protocolAddress,
+                                alertId: 'NETHFORTA-16',
+                                protocol: 'aave',
+                                type: forta_agent_1.FindingType.Suspicious,
+                                severity: forta_agent_1.FindingSeverity.High,
+                                metadata: {
+                                    protocolAddress: protocolAddress,
+                                    balanceDiff: '100000',
+                                    loans: JSON.stringify([flashLoanEvent]),
+                                },
+                            }),
+                        ]);
+                        return [2 /*return*/];
+                }
+            });
+        }); });
+    });
+});
